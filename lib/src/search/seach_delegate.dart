7fc2b8a17447b30b87ab402e599219eacd8f3cfe
Sam Bautista <samuelbaubas@gmail.com>
2020-10-04 19:55:30 -0500

    Se hardcodeo la parte de la busqueda para que podamos impletar los ultimos 2 métodos y con ello hagamos que nos muestre resultados parecidos y que también con ello hagamos que nos muestre el resultado al centro de la pantalla

--- lib/src/search/seach_delegate.dart
+++ lib/src/search/seach_delegate.dart
@@ -2,6 +2,22 @@ import 'package:flutter/material.dart';
 
 class DataSeach extends SearchDelegate {
   //Esta clase va ser el encargado de disparar la acciión en TMovieDB para traernos las peliculas con forme vamos escribiendo algo ahí
+  String seleccion = ' ';
+
+  final peliculas = [
+    'Spiderman',
+    'Aquaman',
+    'Batman',
+    'Shazam!',
+    'Ironman',
+    'Capitan America',
+  ];
+
+  final peliculasRecientes = [
+    'Spiderman',
+    'Capitan America',
+  ];
+
   @override
   List<Widget> buildActions(BuildContext context) {
     //Las acciones de nuestro appBar
@@ -33,12 +49,38 @@ class DataSeach extends SearchDelegate {
   @override
   Widget buildResults(BuildContext context) {
     //Es el builder que crea los resultados que vamos a mostrar
-    return Container();
+    return Center(
+        child: Container(
+      height: 100.0,
+      width: 100.0,
+      color: Colors.greenAccent,
+      child: Text(seleccion),
+    ));
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     //Son las sugerencias que aparencen cuando la persona escribe
-    return Container();
+
+    final listaSugerida = (query.isEmpty)
+        ? peliculasRecientes
+        : peliculas
+            .where((p) => p.toLowerCase().startsWith(query.toLowerCase()))
+            .toList();
+
+    return ListView.builder(
+      itemCount: listaSugerida.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Icon(Icons.movie),
+          title: Text(listaSugerida[index]),
+          onTap: () {
+            seleccion = listaSugerida[index];
+            showResults(
+                context); // con este método que es propio de SearchDelegate podemos hacer que nos ponga el resultado en pantalla con ayuda el método BuildSugestions
+          },
+        );
+      },
+    );
   }
 }
--- lib/src/widgets/card_swiper_widget.dart
+++ lib/src/widgets/card_swiper_widget.dart
@@ -13,38 +13,40 @@ class CardSwiper extends StatelessWidget {
     final _screenSize = MediaQuery.of(context)
         .size; //para hacer la mediaQuery usamos el context es por ello que debe de estar dentro de nuestro método build
 
-    return Container(
-      margin: EdgeInsets.only(
-        top: 2.0,
-        bottom: 0.0,
-      ),
-      child: Swiper(
-        itemWidth: _screenSize.width * 0.7,
-        itemHeight: _screenSize.height * 0.55,
-        layout: SwiperLayout
-            .STACK, //1. para que esta propiedad funcione tenemos que hacer ponerle por lo menos el ancho de las tarjetas
-        itemBuilder: (BuildContext context, int index) {
-          peliculas[index].uniqueId = '${peliculas[index].id}-tarjeta';
-          return Hero(
-            tag: peliculas[index].uniqueId,
-            child: ClipRRect(
-                //espara crear un clip de tarjetas de determinada imagen o cosa
-                borderRadius: BorderRadius.circular(20.0),
-                child: GestureDetector(
-                  onTap: () => Navigator.pushNamed(context, 'detalle',
-                      arguments: peliculas[index]),
-                  child: FadeInImage(
-                    placeholder: AssetImage('assets/img/no-image.jpg'),
-                    image: NetworkImage(peliculas[index].getPosterImg()),
-                    fit: BoxFit.cover,
-                  ),
-                )),
-          );
-        },
-        itemCount: peliculas.length,
-        // pagination:
-        //     new SwiperPagination(), //barra para saber cuantas páginas tengo en el swiper
-        // control: new SwiperControl(), // me indica hacia donde tenemos que girar
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.only(
+          top: 2.0,
+          bottom: 0.0,
+        ),
+        child: Swiper(
+          itemWidth: _screenSize.width * 0.7,
+          itemHeight: _screenSize.height * 0.55,
+          layout: SwiperLayout
+              .STACK, //1. para que esta propiedad funcione tenemos que hacer ponerle por lo menos el ancho de las tarjetas
+          itemBuilder: (BuildContext context, int index) {
+            peliculas[index].uniqueId = '${peliculas[index].id}-tarjeta';
+            return Hero(
+              tag: peliculas[index].uniqueId,
+              child: ClipRRect(
+                  //espara crear un clip de tarjetas de determinada imagen o cosa
+                  borderRadius: BorderRadius.circular(20.0),
+                  child: GestureDetector(
+                    onTap: () => Navigator.pushNamed(context, 'detalle',
+                        arguments: peliculas[index]),
+                    child: FadeInImage(
+                      placeholder: AssetImage('assets/img/no-image.jpg'),
+                      image: NetworkImage(peliculas[index].getPosterImg()),
+                      fit: BoxFit.cover,
+                    ),
+                  )),
+            );
+          },
+          itemCount: peliculas.length,
+          // pagination:
+          //     new SwiperPagination(), //barra para saber cuantas páginas tengo en el swiper
+          // control: new SwiperControl(), // me indica hacia donde tenemos que girar
+        ),
       ),
     );
   }
